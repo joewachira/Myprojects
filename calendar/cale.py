6dafa2e59d624f2ec08eb7e45e05a06e4995a665
joewachira <joewachira49@gmail.com>
2017-10-18 17:56:04 +0300

    add calendar

---
+++ calendar/cale.py
@@ -0,0 +1,15 @@
+# Python program to display calendar of given month of the year
+
+
+import calendar
+
+yy = 2017
+mm = 10
+
+'''
+To ask month and year from the user
+yy = int(input ("Enter year: "))
+mm = int(input ("Enter month: "))
+'''
+# display the calendar
+print(calendar.month(yy, mm))
